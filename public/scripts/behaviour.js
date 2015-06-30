ddedfe4c15a8333a587dcab284434fe5dde0ea63
Hege Røkenes <hege@rokenes.com>
2015-06-30 18:24:39 +0200

    Added Google maps resources
    Currently not working

--- public/scripts/behaviour.js
+++ public/scripts/behaviour.js
@@ -5,3 +5,56 @@ $(function() {
 	$(".hero").css("min-height", documentHeight - headerHeight - 80);
 
 }); // document ready
+
+var map;
+var brooklyn = new google.maps.LatLng(40.6743890, -73.9455);
+
+var MY_MAPTYPE_ID = 'custom_style';
+
+function initialize() {
+
+  var featureOpts = [
+    {
+      stylers: [
+        { hue: '#890000' },
+        { visibility: 'simplified' },
+        { gamma: 0.5 },
+        { weight: 0.5 }
+      ]
+    },
+    {
+      elementType: 'labels',
+      stylers: [
+        { visibility: 'off' }
+      ]
+    },
+    {
+      featureType: 'water',
+      stylers: [
+        { color: '#005589' }
+      ]
+    }
+  ];
+
+  var mapOptions = {
+    zoom: 12,
+    center: brooklyn,
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+    },
+    mapTypeId: MY_MAPTYPE_ID
+  };
+
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+  var styledMapOptions = {
+    name: 'Custom Style'
+  };
+
+  var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+
+  map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
