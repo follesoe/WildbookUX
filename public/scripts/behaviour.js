01ea3ff710d271f1502dee9eb605b6ac94ab071d
Hege Røkenes <hege@rokenes.com>
2015-07-07 12:43:32 +0200

    Hero video suggestion with Raja Ampat video

--- public/scripts/behaviour.js
+++ public/scripts/behaviour.js
@@ -15,9 +15,18 @@ $(function() {
   // Setting hero to window height
 	$(".hero").css("min-height", documentHeight - headerHeight - 80);
 
+  // Watch movie - frontpage
   $("#watch-movie").on("click", function() {
-    $(".hero .container").fadeOut();
+    $(".hero .container").fadeOut("slow");
+    $(".hero .video-wrapper").fadeIn("slow", function() {
+      
+      var vimeoID = "123083341";
+      $(".hero .embed-container").html('<iframe src="https://player.vimeo.com/video/'+ vimeoID +'?portrait=0&title=0&color=bf1f48&badge=0&byline=0&autoplay=1" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>');
+
+    });
   });
+
+  
 
 }); // document ready
 
