9a01cf237feed62d36dd5d914d17ac2900205b86
Hege Røkenes <hege@rokenes.com>
2015-07-07 14:14:37 +0200

    New video test

--- public/scripts/behaviour.js
+++ public/scripts/behaviour.js
@@ -20,8 +20,8 @@ $(function() {
     $(".hero .container").fadeOut("slow");
     $(".hero .video-wrapper").fadeIn("slow", function() {
       
-      var vimeoID = "123083341";
-      $(".hero .embed-container").html('<iframe id="heroVideo" src="https://player.vimeo.com/video/'+ vimeoID +'?api=1&player_id=heroVideo&portrait=0&title=0&color=bf1f48&badge=0&byline=0&autoplay=1" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>');
+      //var vimeoID = "123083341";
+      //$(".hero .embed-container").html('<iframe id="heroVideo" src="https://player.vimeo.com/video/'+ vimeoID +'?api=1&player_id=heroVideo&portrait=0&title=0&color=bf1f48&badge=0&byline=0&autoplay=1" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>');
 
       var iframe = $("#heroVideo")[0],
       player = $f(iframe);
