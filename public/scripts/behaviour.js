cc2b5d9c44621e99ae730a8de5f853b32f1148d8
Hege Røkenes <hege@rokenes.com>
2015-07-07 14:13:02 +0200

    Video finish with new ID

--- public/scripts/behaviour.js
+++ public/scripts/behaviour.js
@@ -21,9 +21,9 @@ $(function() {
     $(".hero .video-wrapper").fadeIn("slow", function() {
       
       var vimeoID = "123083341";
-      $(".hero .embed-container").html('<iframe src="https://player.vimeo.com/video/'+ vimeoID +'?api=1&player_id=video&portrait=0&title=0&color=bf1f48&badge=0&byline=0&autoplay=1" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>');
+      $(".hero .embed-container").html('<iframe id="heroVideo" src="https://player.vimeo.com/video/'+ vimeoID +'?api=1&player_id=heroVideo&portrait=0&title=0&color=bf1f48&badge=0&byline=0&autoplay=1" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>');
 
-      var iframe = $(".hero .embed-container iframe")[0],
+      var iframe = $("#heroVideo")[0],
       player = $f(iframe);
 
       function onFinish(id) {
